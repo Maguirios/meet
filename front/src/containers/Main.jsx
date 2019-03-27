a66f95bca9a2537b3b864f255718a6fe553f77df
Lucas <lucasescudero2013@gmail.com>
2019-03-27 19:50:20 -0300

    conflic merge solved

--- front/src/containers/Main.jsx
+++ front/src/containers/Main.jsx
@@ -1,14 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-<<<<<<< HEAD
-class Main extends React.Component {
-  
-    render() {
-        return (<div> MEET MAIN </div>)
-    }
-}
-=======
 class Main extends Component {
 
 
@@ -31,4 +23,3 @@ const mapDispatchToProps = {
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main)
 
->>>>>>> 480c76e7e6a5ae3a86805db4f3c20a7958cd20c8
