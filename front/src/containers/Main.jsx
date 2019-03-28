f8bf7a232372df563d75150f694e34c4d263655e
Lucas <lucasescudero2013@gmail.com>
2019-03-28 18:08:30 -0300

    se agrego la funcionalidad de registrar usuario en mongo y firebase

--- front/src/containers/Main.jsx
+++ front/src/containers/Main.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Route, Link } from 'react-router-dom'
+import RegisterContainer from './Register'
 
 class Main extends Component {
 
@@ -7,7 +9,8 @@ class Main extends Component {
   render() {
     return (
       <div>
-        hola
+        <Link to='/register'> REGISTRARSE </Link>
+        <Route path='/register' render= {({ history }) => <RegisterContainer  history={history}/>} />
       </div>
     )
   }
