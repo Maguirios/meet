e2584bd31fa1244f955bb9150caf7ce6ff189956
ombaez <ombaez5689@gmail.com>
2019-03-29 16:54:06 -0300

    mongodb out

--- back/app.js
+++ back/app.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const app = express();
 var path = require('path');
-
-//conecting to db
-mongoose.connect('mongodb://localhost/meet',{useNewUrlParser : true})
-.then(db => console.log('db connected'))
-.catch(err => console.log(err))
-
-const apiRoutes = require('./routes/index')
 
 //settings
 app.set('port', process.env.PORT || 3000)
 
 //middleware
 app.use(morgan('dev'))
-
-
-app.use('/api', apiRoutes);
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.resolve(__dirname, 'public')))
 
@@ -28,5 +17,5 @@ app.get('/*', function (req, res) {
 
 
 app.listen(app.get('port'), () => {
-    console.log('Listening on port 4000')
+    console.log('Listening on port 3000')
 })
--- back/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const userRoutes = require('./userRoutes')
-const roomRoutes = require('./roomRoutes')
-
-router.use('/user', userRoutes)
-router.use('/room', roomRoutes)
-
-module.exports = router
