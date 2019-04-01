3b8c219a7b9b6a841b87537b615e1971be75f6c9
Lucas <lucasescudero2013@gmail.com>
2019-04-01 15:44:02 -0300

    debugueo del main listo

--- front/src/containers/Main.jsx
+++ front/src/containers/Main.jsx
@@ -5,11 +5,34 @@ import Button from '@material-ui/core/Button';
 import * as moment from 'moment';
 import RegisterContainer from './Register'
 import Code from '../components/Code';
-import SignIn from "./SignIn"
+import SignIn from "./SignIn";
+import firebase from '../firebase';
 
 
 class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {}
+    }
+    this.signOut = this.signOut.bind(this)
+  }
 
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user })
+      }
+    });
+  }
+  signOut() {
+    firebase.auth().signOut()
+    .then(() => {
+      this.setState({ user: {} })
+    }).catch(function (error) {
+      console.log('El error fue', error)
+    });
+  }
   render() {
     let time = 0
     const update = () => {
@@ -17,30 +40,46 @@ class Main extends Component {
     }
     let newTime = () => {
       setInterval(
-      update()
-    , 1);}
- 
+        update()
+        , 1);
+    }
     newTime()
     const { classes } = this.props;
+    console.log('estadoooo', this.state)
     return (
       <div className='home'>
         <div className='home-top'>
-          <Link to='/signIn'>
-            <Button variant="contained" color="primary">
-              INICIAR SESIÓN
-          </Button>
-          </Link>
-          <Link to='/register'>
-            <Button variant="contained" color="primary" id='register'>
-              REGISTRARSE
-          </Button>
-          </Link>
+          {(this.state.user.email) ?
+            <div className="withUser">
+              <Link to='/createRoom'>
+                <Button variant="contained" color="primary">
+                  CREAR SALA
+                </Button>
+              </Link>
+              <Button variant="contained" color="primary" onClick={this.signOut}>
+                SALIR
+              </Button>
+            </div>
+            :
+            <div className='withoutUser'>
+              <Link to='/signIn'>
+                <Button variant="contained" color="primary">
+                  INICIAR SESIÓN
+                </Button>
+              </Link>
+              <Link to='/register'>
+                <Button variant="contained" color="primary" id='register'>
+                  REGISTRARSE
+                </Button>
+              </Link>
+            </div>
+          }
         </div>
         <div className='home-center'>
           <img className='isologo-horizontal-white' src='/utils/images/logor.png' />
           <div className="components">
-            <Route exact path='/register' render={({ history }) => <RegisterContainer history={history} />} />
-            <Route exact path='/signIn' render={({ history }) => <SignIn history={history} />} />
+            <Route exact path='/register' render={({ history }) => <RegisterContainer history={history} currentUser={this.state.user} />} />
+            <Route exact path='/signIn' render={({ history }) => <SignIn history={history} currentUser={this.state.user}/>} />
             <Route exact path='/' component={Code} />
           </div>
         </div>
