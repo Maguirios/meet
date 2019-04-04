843cd75d67a40b2131e3ce463456f2180459b5fa
ombaez <ombaez5689@gmail.com>
2019-04-04 19:44:17 -0300

    paralademo

--- front/src/containers/Streaming.jsx
+++ front/src/containers/Streaming.jsx
@@ -22,11 +22,7 @@ export default class VideoComponent extends Component {
     this.joinRoom = this.joinRoom.bind(this);
     this.leaveRoom = this.leaveRoom.bind(this);
     this.handleRoomNameChange = this.handleRoomNameChange.bind(this);
-    this.roomJoined = this.roomJoined.bind(this);
-    this.attachTracks = this.attachTracks.bind(this);
-    this.attachParticipantTracks = this.attachParticipantTracks.bind(this);
-    this.detachTracks = this.detachTracks.bind(this);
-    this.detachParticipantTracks = this.detachParticipantTracks.bind(this);
+   
   }
 
   // Busca el token  creado en el back
@@ -41,6 +37,10 @@ export default class VideoComponent extends Component {
   handleRoomNameChange(e) {
     let roomName = e.target.value;
     this.setState({ roomName });
+  }
+
+  handleClick(){
+    console.log(this.state)
   }
   // Se una a la sala
   joinRoom() {
@@ -69,7 +69,7 @@ export default class VideoComponent extends Component {
 
         room.participants.forEach(participantConnected);
         room.on('participantConnected', participantConnected);
-      
+
         room.on('participantDisconnected', participantDisconnected);
         room.once('disconnected', error => room.participants.forEach(participantDisconnected));
       })
@@ -78,21 +78,6 @@ export default class VideoComponent extends Component {
       });
   }
 
-  attachTracks(tracks, container) {
-    tracks.forEach((track) => {
-      console.log("track invisible?", track);
-      container.appendChild(track.attach());
-      console.log("holaa", track);
-    });
-  }
-  
-  //Lo mismo que lo de arriba
-  attachParticipantTracks(participant, container) {
-    console.log(participant);
-    var tracks = Array.from(participant.tracks.values());
-    console.log(tracks);
-    this.attachTracks(tracks, container);
-  }
 
   attachLocalParticipantTracks(participant, container) {
     console.log(participant);
@@ -105,70 +90,7 @@ export default class VideoComponent extends Component {
     });
   }
 
-  //Sigue el curso de la camara
-  roomJoined(room) {
-    console.log("Joined as '" + this.state.identity + "'");
-    this.setState({
-      activeRoom: room,
-      localMediaAvailable: true,
-      hasJoinedRoom: true
-    });
-
-    var previewContainer = this.refs.localMedia;
-    if (!previewContainer.querySelector("video")) {
-      console.log('Local')
-      this.attachLocalParticipantTracks(room.localParticipant, previewContainer);
-    }
-
-    room.participants.forEach(participant => {
-      var previewContainer = this.refs.remoteMedia;
-      this.attachParticipantTracks(participant, previewContainer);
-    });
-
-    room.on('participantConnected', participant => {
-      console.log("Joining: '" + participant.identity + "'");
-    });
-
-    room.on('trackAdded', (track, participant) => this.attachTracks([track], this.refs.remoteMedia));
-
-
-    room.on("trackRemoved", (track, participant) => {
-      this.log(participant.identity + " removed track: " + track.kind);
-      this.detachTracks([track]);
-    });
-
-    room.on("participantDisconnected", participant => {
-      console.log("Participant '" + participant.identity + "' left the room");
-      this.detachParticipantTracks(participant);
-    });
-    room.on("disconnected", () => {
-      if (this.state.previewTracks) {
-        this.state.previewTracks.forEach(track => {
-          track.stop();
-        });
-      }
-      this.detachParticipantTracks(room.localParticipant);
-      room.participants.forEach(this.detachParticipantTracks);
-      this.state.activeRoom = null;
-      this.setState({ hasJoinedRoom: false, localMediaAvailable: false });
-    });
-  }
-  //{Participantes}
-  detachTracks(tracks) {
-    tracks.forEach(track => {
-      console.log("holaaaaaquivaa");
-      track.track.detach().forEach(detachedElement => {
-        detachedElement.remove();
-      });
-    });
-  }
-  //{Participantes}
-  detachParticipantTracks(participant) {
-    console.log("holaa aqui fuee");
-    var tracks = Array.from(participant.tracks.values());
-    this.detachTracks(tracks);
-  }
-
+  
 
   //Sales de la sala
   leaveRoom() {
@@ -184,8 +106,8 @@ export default class VideoComponent extends Component {
         <div ref="localMedia" />{" "}
       </div>
     ) : (
-      ""
-    );
+        ""
+      );
 
     let joinOrLeaveRoomButton = this.state.hasJoinedRoom ? (
       <RaisedButton
@@ -194,8 +116,8 @@ export default class VideoComponent extends Component {
         onClick={this.leaveRoom}
       />
     ) : (
-      <RaisedButton label="Join Room" primary={true} onClick={this.joinRoom} />
-    );
+        <RaisedButton label="Join Room" primary={true} onClick={this.joinRoom} />
+      );
 
     return (
       <Card>
@@ -210,7 +132,7 @@ export default class VideoComponent extends Component {
                   this.state.roomNameErr ? "Room Name is required" : undefined
                 }
               />
-                                                                                                                                                  
+
               <div>
                 {
                   // this.props.participants.map((participant) => <div id={} ref="" />)
@@ -218,9 +140,16 @@ export default class VideoComponent extends Component {
                 }
               </div>
 
+              <div>
 
-              <div ref="localMedia" />
-           |
+                <div ref="localMedia" id='local-media' />
+                <div id='totalRemote'>
+
+                    <div ref="remoteMedia" id='remote-media' />
+ 
+                </div>
+
+              </div>
             </div>
             <br />
             {joinOrLeaveRoomButton}
@@ -231,34 +160,35 @@ export default class VideoComponent extends Component {
   }
 }
 
- 
+
 function participantConnected(participant) {
-  
+
   const div = document.createElement('div');
   div.id = participant.sid;
   div.innerText = participant.identity;
- 
+
   participant.on('trackSubscribed', track => trackSubscribed(div, track));
   participant.on('trackUnsubscribed', trackUnsubscribed);
- 
+
   participant.tracks.forEach(publication => {
     if (publication.isSubscribed) {
       trackSubscribed(div, publication.track);
     }
   });
   let remoteMedias = document.getElementById('remote-media');
+  remoteMedias.style.height = '150px'
   remoteMedias.appendChild(div);
 }
- 
+
 function participantDisconnected(participant) {
   console.log('Participant "%s" disconnected', participant.identity);
   document.getElementById(participant.sid).remove();
 }
- 
+
 function trackSubscribed(div, track) {
   div.appendChild(track.attach());
 }
- 
+
 function trackUnsubscribed(track) {
   track.detach().forEach(element => element.remove());
 }
