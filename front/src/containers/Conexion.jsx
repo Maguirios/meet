bc90ec49bc237a0b5282a8d21b6d8a89c769a5c5
angel-er <a.encinasrm@gmail.com>
2019-04-04 12:38:44 -0300

    conexiones

---
+++ front/src/containers/Conexion.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Conexion = () => {
+	return (
+		<div className='iconos'>
+			<div>
+				<img className='watermark' src='/utils/images/logor.png' />
+			</div>
+
+			<h4 id='text-permisos'>Estableciendo conexión...</h4>
+		</div>
+	)
+}
+
+export default Conexion;
