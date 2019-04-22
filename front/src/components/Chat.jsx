d522d2faf976d3d53e89c5377f61e4c302601c03
Javier <javilanger@gmail.com>
2019-04-22 19:30:30 -0300

    chat unio a la sala

--- front/src/components/Chat.jsx
+++ front/src/components/Chat.jsx
@@ -151,7 +151,7 @@ class Chat extends React.Component {
     this.handleChangeMessage = this.handleChangeMessage.bind(this)
   }
   componentDidMount() {
-    firebase.database().ref(`rooms/${this.props.room}/messages/`).on('value', snapshoot => {
+    this.chat = firebase.database().ref(`rooms/${this.props.room}/messages/`).on('value', snapshoot => {
       const actMsj = snapshoot.val()
       if (actMsj !== null) {
         this.setState({
@@ -170,6 +170,10 @@ class Chat extends React.Component {
       var show = document.getElementById('style-1').lastChild;
       show && show.scrollIntoView(false)
     }
+  }
+
+  componentWillUnmount(){
+    firebase.database().ref(`rooms/${this.props.room}/messages/`).off('value', this.chat)
   }
 
   handleDownload(fileName) {
